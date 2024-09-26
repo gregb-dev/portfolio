7e2a9550f3e07605d932320b381437bf8991ed2a
gregb-dev <greg.berthold@outlook.com>
2024-09-26 12:16:35 -0400

    Add grid layout to display projects in Work component.

--- gb-portfolio/src/components/content/work/Work.tsx
+++ gb-portfolio/src/components/content/work/Work.tsx
@@ -3,6 +3,7 @@ import { useTranslation } from 'react-i18next';
 import ProjectCard from '../../cards/project-card/ProjectCard';
 import RecycMontrealMockup01 from '../../../assets/images/project-recycmontreal/iphone16-mock01.png';
 import RecycMontrealMockup02 from '../../../assets/images/project-recycmontreal/iphone16-mock02.png';
+import EcoleEnFolieMockup from '../../../assets/images/project-ecoleenfolie/macbook-mock.png';
 
 function Work() {
 
@@ -13,26 +14,31 @@ function Work() {
             <div id='work'>
                 <div className='content work-container'>
                     <h2 className='subheading'><span className='subheading-bold'>{t('headings.work')}</span> | {t('headings.workSubheading')}</h2>
-                    <div>
-                        <ProjectCard
-                            name={t('projectCardRecycMontreal.name')}
-                            type={t('projectCardRecycMontreal.type')}
-                            description={t('projectCardRecycMontreal.description')}
-                            imageUrl1={RecycMontrealMockup01}
-                            imageUrl2={RecycMontrealMockup02}
-                            imageAlt={t('projectCardRecycMontreal.imageAlt')}
-                            href={t('projectCardRecycMontreal.href')}
-                        />
+                    <div className='projects-layout'>
+                        <div className='project-card-container'>
+                            <ProjectCard
+                                name={t('projectCardRecycMontreal.name')}
+                                type={t('projectCardRecycMontreal.type')}
+                                description={t('projectCardRecycMontreal.description')}
+                                imageUrl1={RecycMontrealMockup01}
+                                imageUrl2={RecycMontrealMockup02}
+                                imageAlt={t('projectCardRecycMontreal.imageAlt')}
+                                href={t('projectCardRecycMontreal.href')}
+                                case='mobile'
+                            />
+                        </div>
+                        <div className='project-card-container'>
+                            <ProjectCard
+                                name={t('projectCardEcoleEnFolie.name')}
+                                type={t('projectCardEcoleEnFolie.type')}
+                                description={t('projectCardEcoleEnFolie.description')}
+                                imageUrl1={EcoleEnFolieMockup}
+                                imageAlt={t('projectCardEcoleEnFolie.imageAlt')}
+                                href={t('projectEcoleEnFolie.href')}
+                                case='web'
+                            />
+                        </div>
                     </div>
-
-                    {/*<ProjectCard
-                        name={t('projectEcoleEnFolie.name')}
-                        type={t('projectEcoleEnFolie.type')}
-                        description={t('projectEcoleEnFolie.description')}
-                        imageUrl={t('projectEcoleEnFolie.imageUrl')}
-                        imageAlt={t('projectEcoleEnFolie.imageAlt')}
-                        href={t('projectEcoleEnFolie.href')}
-                    />*/}
                 </div>
             </div>
         </>
