a1219a47be74de5e6763cdfe720f3073476ca480
gregb-dev <greg.berthold@outlook.com>
2024-09-26 18:00:28 -0400

    Reformat the structure of the elements of the Footer component.

--- gb-portfolio/src/components/footer/Footer.tsx
+++ gb-portfolio/src/components/footer/Footer.tsx
@@ -1,6 +1,7 @@
 import './Footer.css';
 import { useTranslation } from 'react-i18next';
 import Socials from '../menus/socials/Socials';
+import FooterLogo from '../../assets/logos/gb/gb-logomark-white.svg';
 
 function Footer() {
 
@@ -10,29 +11,31 @@ function Footer() {
         <>
             <div id='footer'>
                 <div className='content footer-container'>
-                    <div>
-                        <ul>
-                            <li>B</li>
-                            <li>Greg Berthold</li>
-                        </ul>
-                        <ul>
-                            <li>{t('menu.home')}</li>
-                            <li>{t('menu.work')}</li>
-                            <li>{t('menu.about')}</li>
-                            <li>{t('menu.contact')}</li>
-                            <ul>
-                                <li>{t('menu.multiLang.english')}</li>
-                                <li>{t('menu.multiLang.french')}</li>
-                            </ul>
-                        </ul>
-                        <Socials
-                            isDarkIcons={false}
-                            size='36'
-                        />
-                    </div>
-                    <div>
-                        <p>{t('funFact.label')} : {t('funFact.message')}</p>
-                        <p>{t('copyrights')}</p>
+                    <div className='footer-border'>
+                        <img className='footer-logo' src={FooterLogo} alt='Gb Logo' />
+                        <div className='footer'>
+                            <div className='footer-nav'>
+                                <ul className='footer-nav-site'>
+                                    <li>{t('menu.home')}</li>
+                                    <li>{t('menu.work')}</li>
+                                    <li>{t('menu.about')}</li>
+                                    <li>{t('menu.contact')}</li>
+                                </ul>
+                                <div className='footer-nav-socials'>
+                                    <Socials
+                                        isDarkIcons={false}
+                                        size='36'
+                                    />
+                                </div>
+                                <ul className='footer-nav-multilang'>
+                                    <li>{t('menu.multiLang.english')}</li>
+                                    <li>{t('menu.multiLang.french')}</li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div>
+                            <p>{t('copyrights')}</p>
+                        </div>
                     </div>
                 </div>
             </div>
