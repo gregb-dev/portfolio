27285fcf71a83a8752e3255038b747995b8b839e
gregb-dev <greg.berthold@outlook.com>
2024-09-26 15:40:51 -0400

    Add new logo color (#fffffc) anf fix original version color (#000000 to #0c1713).

--- gb-portfolio/src/components/footer/Footer.tsx
+++ gb-portfolio/src/components/footer/Footer.tsx
@@ -1,6 +1,6 @@
-import Socials from '../menus/socials/Socials';
 import './Footer.css';
 import { useTranslation } from 'react-i18next';
+import Socials from '../menus/socials/Socials';
 
 function Footer() {
 
@@ -8,27 +8,29 @@ function Footer() {
 
     return (
         <>
-            <div id="footer" className='full-height'>
-                <div>
-                    <ul>
-                        <li>B</li>
-                        <li>Greg Berthold</li>
-                    </ul>
-                    <ul>
-                        <li>{t('menu.home')}</li>
-                        <li>{t('menu.work')}</li>
-                        <li>{t('menu.about')}</li>
-                        <li>{t('menu.contact')}</li>
+            <div id='footer'>
+                <div className='content footer-container'>
+                    <div>
                         <ul>
-                            <li>{t('menu.languages.english')}</li>
-                            <li>{t('menu.languages.french')}</li>
+                            <li>B</li>
+                            <li>Greg Berthold</li>
                         </ul>
-                    </ul>
-                    <Socials />
-                </div>
-                <div>
-                    <p>{t('funFact.label')} : {t('funFact.message')}</p>
-                    <p>{t('copyrights')}</p>
+                        <ul>
+                            <li>{t('menu.home')}</li>
+                            <li>{t('menu.work')}</li>
+                            <li>{t('menu.about')}</li>
+                            <li>{t('menu.contact')}</li>
+                            <ul>
+                                <li>{t('menu.multiLang.english')}</li>
+                                <li>{t('menu.multiLang.french')}</li>
+                            </ul>
+                        </ul>
+                        <Socials />
+                    </div>
+                    <div>
+                        <p>{t('funFact.label')} : {t('funFact.message')}</p>
+                        <p>{t('copyrights')}</p>
+                    </div>
                 </div>
             </div>
         </>
