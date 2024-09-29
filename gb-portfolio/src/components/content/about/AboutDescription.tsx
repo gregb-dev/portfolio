e4e1ec8517a950453bc8a063542a12ccfc230170
gregb-dev <greg.berthold@outlook.com>
2024-09-29 12:46:18 -0400

    Add mockup text and styles to the About Me page components.

--- gb-portfolio/src/components/content/about/AboutDescription.tsx
+++ gb-portfolio/src/components/content/about/AboutDescription.tsx
@@ -1,12 +1,34 @@
 import './AboutDescription.css';
+import { useTranslation } from 'react-i18next';
+import ProfilePicture from '../../../assets/logos/gb/gb-logomark.svg';
 
 function AboutBanner() {
+
+    const { t } = useTranslation();
 
     return (
         <>
             <div className='content about-description-container'>
-                <div className='about-description'>
-                    <h1>About me</h1>
+                <div className='about-description site-width'>
+                    <div className='about-description-profile'>
+                        <div className='about-description-pfp-container'>
+                        </div>
+                        <div className='about-description-text'>
+                            <h1>MY JOURNEY</h1>
+                            <p>{t('loremIpsum.part1')}</p>
+                        </div>
+                    </div>
+                    <div className='about-about-description-workflow'>
+                        <h1>WORKFLOW</h1>
+                        <p>{t('loremIpsum.part2')}</p>
+                        <div className='about-about-description-workflow-chart'>
+                            <ul>
+                                <li>Text</li>
+                                <li>|</li>
+                                <li>Text</li>
+                            </ul>
+                        </div>
+                    </div>
                 </div>
             </div>
         </>
