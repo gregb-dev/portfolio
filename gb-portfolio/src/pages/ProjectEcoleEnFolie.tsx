682c4ae4fc9230a897049a01233663ba31e5b58b
gregb-dev <greg.berthold@outlook.com>
2024-09-29 22:36:26 -0400

    Fix unused lines in order to create preview build on Vercel.

--- gb-portfolio/src/pages/ProjectEcoleEnFolie.tsx
+++ gb-portfolio/src/pages/ProjectEcoleEnFolie.tsx
@@ -1,8 +1,8 @@
-import { useTranslation } from 'react-i18next';
+//import { useTranslation } from 'react-i18next';
 
 function ProjectEcoleEnFolie() {
 
-    const { t } = useTranslation();
+    //const { t } = useTranslation();
 
     return (
         <>
