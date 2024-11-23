8e6ca8d53e3ce9512655be12a75b130fd2d657d9
gregb-dev <greg.berthold@outlook.com>
2024-11-23 18:01:13 -0500

    Add styles to ProjectHeader component.

--- gb-portfolio/src/components/banner/ProjectHeader.tsx
+++ gb-portfolio/src/components/banner/ProjectHeader.tsx
@@ -1,4 +1,5 @@
 import './ProjectHeader.css';
+import { useTranslation } from 'react-i18next';
 
 interface ProjectHeaderProps {
     name: string,
@@ -14,6 +15,8 @@ interface ProjectHeaderProps {
 
 function ProjectHeader(props: ProjectHeaderProps) {
 
+    const { t } = useTranslation();
+
     return (
         <>
             <div className='project-header'>
@@ -22,22 +25,26 @@ function ProjectHeader(props: ProjectHeaderProps) {
                     {props.tag && (<h2>{props.tag}</h2>)}
                     <ul>
                         <li>
-                            <h3>Overview</h3>
+                            <h3>{t('projectHeaderLabels.overview')}</h3>
                             <p>{props.description}</p>
                         </li>
                         <li>
-                            <h3>Role</h3>
+                            <h3>{t('projectHeaderLabels.role')}</h3>
                             <p>{props.role}</p>
                         </li>
                         <li>
-                            <h3>Timeline</h3>
+                            <h3>{t('projectHeaderLabels.timeline')}</h3>
                             <p>{props.timeline}</p>
                         </li>
                     </ul>
                 </div>
-                <div className='projet-header-image-container'>
-
-                </div>
+                {props.case === 'web' && (
+                    <div className='projet-header-image-container'>
+                        <div className='projet-header-laptop'>
+                            <img className='projet-header-device-img' src={props.imageUrl1} alt={props.imageAlt} />
+                        </div>
+                    </div>
+                )}
             </div>
         </>
     )
